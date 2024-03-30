487041ed1a5b3757d15b4e99d1cf7bf2c38532e8
thayrov <thayrovg@gmail.com>
2024-03-30 15:31:30 -0600

    feat: :sparkles: refactor code and add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/Base.styles.jsx';
+import { AppDiv, MainTitle } from './styles/app/App.styles';
+import GlobalStyle from './styles/GlobalStyle.js';
+import Blob from './components/Blob.js';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <AppDiv>
+        <GlobalStyle />
+        <Blob />
+        <MainTitle>
+          RICK <span>&&</span> MORTY
+        </MainTitle>
+      </AppDiv>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
