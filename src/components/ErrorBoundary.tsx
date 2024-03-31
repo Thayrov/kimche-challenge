55fd1b41683f31b535ce4d7d661b09339507bb9f
thayrov <thayrovg@gmail.com>
2024-03-31 15:26:17 -0600

    feat: :sparkles: add ErrorBoundary component

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,23 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return (this.props as { children: ReactNode }).children;
+  }
+}
+
+export default ErrorBoundary;
