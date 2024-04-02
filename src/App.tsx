f5d023fdee34cb76996c3e60d3761b1375aa7502
thayrov <thayrovg@gmail.com>
2024-04-02 14:58:45 -0600

    feat: :sparkles: add search functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,19 @@ import { AppDiv, MainTitle } from './styles/app/App.styles';
 import GlobalStyle from './styles/GlobalStyle.js';
 import Blob from './components/Blob.js';
 import Cards from './components/Cards.js';
+import { GET_CHARACTERS } from './graphql/queries.js';
+import { useState } from 'react';
+import { useLazyQuery } from '@apollo/client';
+import SearchBar from './components/SearchBar.js';
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [getCharacters] = useLazyQuery(GET_CHARACTERS, {
+    onCompleted: (data) => {
+      setSearchResults(data.characters.results);
+    },
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <AppDiv>
@@ -14,7 +25,8 @@ function App() {
         <MainTitle>
           RICK <span>&&</span> MORTY
         </MainTitle>
-        <Cards />
+        <SearchBar getCharacters={getCharacters} />
+        <Cards searchResults={searchResults} />
       </AppDiv>
     </ThemeProvider>
   );
